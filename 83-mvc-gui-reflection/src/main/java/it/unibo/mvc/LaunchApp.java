76c9561bd27369463c27c4c54b180c3515132eca
Elettra Ventura <even2728@gmail.com>
2024-11-13 19:01:11 +0000

    committing solutions

--- 83-mvc-gui-reflection/src/main/java/it/unibo/mvc/LaunchApp.java
+++ 83-mvc-gui-reflection/src/main/java/it/unibo/mvc/LaunchApp.java
@@ -13,29 +13,29 @@ import java.util.List;
  */
 public final class LaunchApp {
 
-    private LaunchApp() { }
+    private LaunchApp() {
+    }
 
     /**
      * Runs the application.
      *
      * @param args ignored
-     * @throws ClassNotFoundException if the fetches class does not exist
-     * @throws NoSuchMethodException if the 0-ary constructor do not exist
+     * @throws ClassNotFoundException    if the fetches class does not exist
+     * @throws NoSuchMethodException     if the 0-ary constructor do not exist
      * @throws InvocationTargetException if the constructor throws exceptions
-     * @throws InstantiationException if the constructor throws exceptions
-     * @throws IllegalAccessException in case of reflection issues
-     * @throws IllegalArgumentException in case of reflection issues
+     * @throws InstantiationException    if the constructor throws exceptions
+     * @throws IllegalAccessException    in case of reflection issues
+     * @throws IllegalArgumentException  in case of reflection issues
      */
     public static void main(final String... args)
-        throws
-        ClassNotFoundException,
-        NoSuchMethodException,
-        InvocationTargetException,
-        InstantiationException,
-        IllegalAccessException {
+            throws ClassNotFoundException,
+            NoSuchMethodException,
+            InvocationTargetException,
+            InstantiationException,
+            IllegalAccessException {
         final var model = new DrawNumberImpl();
         final DrawNumberController app = new DrawNumberControllerImpl(model);
-        for (final var viewType: List.of("StandardOutput", "Swing")) {
+        for (final var viewType : List.of("StandardOutput", "Swing")) {
             final var clazz = Class.forName("it.unibo.mvc.view.DrawNumber" + viewType + "View");
             for (int i = 0; i < 3; i++) {
                 final var newView = clazz.getConstructor().newInstance();
@@ -43,8 +43,7 @@ public final class LaunchApp {
                     app.addView((DrawNumberView) newView);
                 } else {
                     throw new IllegalStateException(
-                        newView.getClass() + " is not a subclass of " + DrawNumberView.class
-                    );
+                            newView.getClass() + " is not a subclass of " + DrawNumberView.class);
                 }
             }
         }
